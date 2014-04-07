bb180b7e5a5823d5584767e1022c338b7f82dbe6
Aleksandr Ivanov <alexander.ivanoff@gmail.com>
2014-04-07 11:27:11 +0400

    FileSystem Storage by default

--- src/org/onepf/repository/model/RepositoryConfigurator.java
+++ src/org/onepf/repository/model/RepositoryConfigurator.java
@@ -3,8 +3,8 @@ package org.onepf.repository.model;
 import org.onepf.repository.model.auth.AppstoreAuthenticator;
 import org.onepf.repository.model.services.DataService;
 import org.onepf.repository.model.services.StorageService;
-import org.onepf.repository.model.services.amazon.AmazonOptions;
-import org.onepf.repository.model.services.amazon.AmazonStorageService;
+import org.onepf.repository.model.services.filesystem.FilesystemOptions;
+import org.onepf.repository.model.services.filesystem.FilesystemStorageService;
 import org.onepf.repository.model.services.mysql.SqlDataService;
 import org.onepf.repository.model.services.mysql.SqlOptions;
 
@@ -28,7 +28,8 @@ public class RepositoryConfigurator {
     public static synchronized RepositoryFactory getRepositoryFactory(ServletContext context) {
         if (repositoryFactory == null) {
             dataService = new SqlDataService(new SqlOptions());
-            storageService = new AmazonStorageService(new AmazonOptions());
+            //storageService = new AmazonStorageService(new AmazonOptions());
+            storageService = new FilesystemStorageService(new FilesystemOptions(context));
             repositoryFactory = new RepositoryFactory(dataService, storageService);
         }
         return repositoryFactory;
