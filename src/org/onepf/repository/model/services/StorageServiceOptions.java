b9ab2a2851ebe1f142526ab90ffd3c9757135e5c
Aleksandr Ivanov <alexander.ivanoff@gmail.com>
2014-04-11 15:02:51 +0400

    javadoc

--- src/org/onepf/repository/model/services/StorageServiceOptions.java
+++ src/org/onepf/repository/model/services/StorageServiceOptions.java
@@ -3,10 +3,16 @@ package org.onepf.repository.model.services;
 /**
  * Abstraction of options object to create and configure storage service
  *
-* @author Alexander Ivanoff on 07.04.14.
+* @author Alexander Ivanoff
  */
 public interface StorageServiceOptions {
 
+    /**
+     * Create Right Options object. Must be overriden in childs.
+     *
+     * @return created storage service
+     *
+     * @see org.onepf.repository.model.services.StorageService
+     */
     StorageService createStorageService();
-
 }
